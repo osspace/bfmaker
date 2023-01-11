6fb4b4e2993508cdda90640732bdd0b54243c684
DoIdo <doidoanny@qq.com>
2023-01-11 23:16:54 +0800

    优化代码结构

--- source/bitmap_font.cpp
+++ source/bitmap_font.cpp
@@ -1,54 +1,35 @@
 #include "bitmap_font.h"
 #include <iostream>
 
-BitmapFont::BitmapFont(size_t row, size_t col) {
-    row_ = row;
-    col_ = col;
-    size_t count = row * col;
-    bitmap_.resize((count + 7) / 8, 0);
-}
-
-BitmapFont::BitmapFont(const FT_Bitmap& ft_bitmap) {
-    FT_Int rows = ft_bitmap.rows;
-    FT_Int cols = ft_bitmap.width;
-    *this = BitmapFont(rows, cols);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (ft_bitmap.buffer[i * cols + j])
-                set(i, j);
-        }
-    }
+BitmapFont::BitmapFont(int32_t pixel) {
+    pixel_ = pixel;
+    bitmap_.resize((pixel * pixel + 7) / 8, 0);
 }
 
 BitmapFont::~BitmapFont() {}
 
-size_t BitmapFont::row() const {
-    return row_;
-}
-
-size_t BitmapFont::col() const {
-    return col_;
-}
-
-size_t BitmapFont::byte_size() const {
-    return bitmap_.size();
-}
-
 void BitmapFont::set(size_t i, size_t j) {
-    size_t pos = i * col_ + j;
+    size_t pos = i * pixel_ + j;
     size_t base = pos / 8;
     size_t offset = pos % 8;
     bitmap_[base] |= (1 << offset);
 }
 
-void BitmapFont::set_bitmap(uint8_t* byte_stream) {
-    for (int i = 0; i < bitmap_.size(); i++) {
-        bitmap_[i] = byte_stream[i];
+void BitmapFont::set_bitmap(const FT_Bitmap& ft_bitmap, int delta_x, int delta_y) {
+    int rows = ft_bitmap.rows;
+    int cols = ft_bitmap.width;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (ft_bitmap.buffer[i * cols + j]) {
+                set(i + delta_y, j + delta_x);
+            }
+        }
     }
 }
 
 bool BitmapFont::value(size_t i, size_t j) const {
-    size_t pos = i * col_ + j;
+    size_t pos = i * pixel_ + j;
     size_t base = pos / 8;
     size_t offset = pos % 8;
     return bitmap_[base] & (1 << offset);
@@ -74,10 +55,9 @@ std::ofstream& operator<<(std::ofstream& out_fstream, const BitmapFont& font_bit
 }
 
 void BitmapFont::show() const {
-    std::cout << (int)this->delta_x << " " << (int)this->delta_y << std::endl;
-    for (size_t i = 0; i < row_; i++) {
-        for (size_t j = 0; j < col_; j++) {
-            putchar(value(i, j) == false ? ' ' : '*');
+    for (size_t i = 0; i < pixel_; i++) {
+        for (size_t j = 0; j < pixel_; j++) {
+            putchar(value(i, j) == false ? '0' : '*');
         }
         putchar('\n');
     }
--- source/bitmap_font_maker.cpp
+++ source/bitmap_font_maker.cpp
@@ -25,15 +25,38 @@ BitmapFont BitmapFontMaker::get_unicode_bitmap(uint16_t unicode, int32_t pixel) 
         pixel_to_max_bearing_y[pixel] = get_max_bearing_y(pixel);
     int32_t max_bearing_y = pixel_to_max_bearing_y[pixel];
 
-    if (FT_Set_Char_Size(face_, FT_F26Dot6(pixel << 6), FT_F26Dot6(pixel << 6), 72, 72))
+    if (FT_Set_Pixel_Sizes(face_, pixel, pixel))
         exit(1);
-    slot_ = face_->glyph;
-    if (FT_Load_Char(face_, unicode, FT_LOAD_RENDER))
+    if (FT_Load_Char(face_, unicode,
+                     /*FT_LOAD_RENDER|*/ FT_LOAD_FORCE_AUTOHINT |
+                         (true ? FT_LOAD_TARGET_NORMAL : FT_LOAD_MONOCHROME | FT_LOAD_TARGET_MONO))) {
+        exit(1);
+    }
+    // 得到字模
+    FT_Glyph glyph;
+    // 把字形图像从字形槽复制到新的FT_Glyph对象glyph中。这个函数返回一个错误码并且设置glyph。
+    if (FT_Get_Glyph(face_->glyph, &glyph))
         exit(1);
 
-    BitmapFont bitmap = BitmapFont(face_->glyph->bitmap);
-    bitmap.delta_x = face_->glyph->bitmap_left;                      // 计算x的偏移量
-    bitmap.delta_y = max_bearing_y - face_->glyph->bitmap_top - 1;   // 利用最大值计算y的偏移量
+    FT_BBox bbox;
+    FT_Glyph_Get_CBox(glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox);
+
+    int32_t height = face_->glyph->bitmap.rows;
+    int32_t width = face_->glyph->bitmap.width;
+    int32_t box_height = bbox.yMax - bbox.yMin + 1;
+    // if (height > pixel || bbox.yMin >= 0 || width > pixel || box_height > pixel) {
+    //     return BitmapFont(pixel);
+    // }
+
+    int32_t delta_x = face_->glyph->bitmap_left;   // 计算x的偏移量
+    int32_t delta_y = max_bearing_y - bbox.yMax;   // 利用最大值计算y的偏移量
+    std::cout << delta_x << " " << delta_y << std::endl;
+    std::cout << max_bearing_y << std::endl;
+    std::cout << height << " " << width << std::endl;
+    BitmapFont bitmap = BitmapFont(pixel);
+    bitmap.set_bitmap(face_->glyph->bitmap, delta_x, delta_y);
+
+    std::cout << 1 << std::endl;
     return bitmap;
 }
 
@@ -42,11 +65,29 @@ int32_t BitmapFontMaker::get_max_bearing_y(int32_t pixel) {
     for (uint16_t unicode = 0; unicode < static_cast<uint16_t>(-1); unicode++) {
         if (FT_Set_Pixel_Sizes(face_, pixel, pixel))
             exit(1);
-
-        if (FT_Load_Char(face_, unicode, FT_LOAD_RENDER))
+        if (FT_Load_Char(face_, unicode,
+                         /*FT_LOAD_RENDER|*/ FT_LOAD_FORCE_AUTOHINT |
+                             (true ? FT_LOAD_TARGET_NORMAL : FT_LOAD_MONOCHROME | FT_LOAD_TARGET_MONO))) {
             exit(1);
+        }
+        // 得到字模
+        FT_Glyph glyph;
+        // 把字形图像从字形槽复制到新的FT_Glyph对象glyph中。这个函数返回一个错误码并且设置glyph。
+        if (FT_Get_Glyph(face_->glyph, &glyph))
+            exit(1);
+        FT_BBox bbox;
+        FT_Glyph_Get_CBox(glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox);
 
-        max_bearing_y = std::max(max_bearing_y, face_->glyph->bitmap_top);
+        int32_t height = face_->glyph->bitmap.rows;
+        int32_t width = face_->glyph->bitmap.width;
+        int32_t box_height = bbox.yMax - bbox.yMin + 1;
+        // if (height > pixel || bbox.yMin >= 0 || width > pixel || box_height > pixel)
+        //     continue;
+        max_bearing_y = std::max(max_bearing_y, (int)face_->glyph->bitmap.rows);
+        if (max_bearing_y == 18) {
+            std::cout << bbox.yMax << " " << bbox.yMin << std::endl;
+            exit(0);
+        }
     }
     return max_bearing_y;
 }
